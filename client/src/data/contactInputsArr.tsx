72274ba7a8fd5eae3a09e1cc1cd6fd4ba086b2b8
dikshit673 <sachinnavodian@gmail.com>
2025-05-31 09:56:02 +0530

    [update]:added contact data for generating form

---
+++ client/src/data/contactInputsArr.tsx
@@ -0,0 +1,46 @@
+import { ContactFormKeysType } from '@/types/Contact';
+import { InputItemType, InputTagType, InputType } from '@/types/Input';
+
+interface ContactItemType extends InputItemType {
+  type: InputType;
+  name: ContactFormKeysType;
+  isInput?: InputTagType;
+}
+
+const contactInputsArr: ContactItemType[] = [
+  {
+    id: 'field-1',
+    type: 'text',
+    name: 'name',
+    label: 'Name',
+    placeholder: '',
+    required: true,
+  },
+  {
+    id: 'field-2',
+    type: 'email',
+    name: 'email',
+    label: 'Email',
+    placeholder: '',
+    required: true,
+  },
+  {
+    id: 'field-3',
+    type: 'text',
+    name: 'subject',
+    label: 'Subject',
+    placeholder: '',
+    required: true,
+  },
+  {
+    id: 'field-4',
+    type: 'text',
+    name: 'message',
+    label: 'Message',
+    placeholder: '',
+    required: true,
+    isInput: 'textarea',
+  },
+];
+
+export { contactInputsArr };
