ea9ad391154ceb7595316f47b56ce670e278259c
dikshit673 <sachinnavodian@gmail.com>
2025-05-31 09:52:57 +0530

    {update}:added kitchen staff data

---
+++ client/src/data/kitchenStaffsArr.tsx
@@ -0,0 +1,85 @@
+interface kitchenStaffsItemType {
+  id: number;
+  name: string;
+  imgSrc: string;
+  alt: string;
+  description: string;
+  position: string;
+}
+
+const kitchenStaffsArr: kitchenStaffsItemType[] = [
+  {
+    id: 1,
+    name: 'david miller',
+    imgSrc:
+      'https://images.unsplash.com/photo-1574966740793-953ad374e8fe?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'kitchen staff 1',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    position: 'chef',
+  },
+  {
+    id: 2,
+    name: 'stafeny rose',
+    imgSrc:
+      'https://images.unsplash.com/photo-1659354213732-8cdec0a6fb47?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'kitchen staff 2',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    position: 'head cook',
+  },
+  {
+    id: 3,
+    name: 'micheal smith',
+    imgSrc:
+      'https://images.unsplash.com/photo-1659354213806-8432b7848a7e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'kitchen staff 3',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    position: 'food supervisor',
+  },
+  {
+    id: 4,
+    name: 'stephen miller',
+    imgSrc:
+      'https://images.unsplash.com/photo-1570568348726-8966d0e61729?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'kitchen staff 4',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    position: 'food supervisor',
+  },
+  {
+    id: 5,
+    name: 'andy smith',
+    imgSrc:
+      'https://images.unsplash.com/photo-1574966740793-953ad374e8fe?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'kitchen staff 1',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    position: 'chef',
+  },
+  {
+    id: 6,
+    name: 'angilina rose',
+    imgSrc:
+      'https://images.unsplash.com/photo-1659354213732-8cdec0a6fb47?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'kitchen staff 2',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    position: 'head cook',
+  },
+  {
+    id: 7,
+    name: 'micheal smith',
+    imgSrc:
+      'https://images.unsplash.com/photo-1659354213806-8432b7848a7e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'kitchen staff 3',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    position: 'food supervisor',
+  },
+  {
+    id: 8,
+    name: 'billy rose',
+    imgSrc:
+      'https://images.unsplash.com/photo-1570568348726-8966d0e61729?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'kitchen staff 4',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    position: 'food supervisor',
+  },
+];
+
+export { kitchenStaffsArr };
