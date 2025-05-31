e278fbab5cb2334b66d553c23f3ba6d4bc8250dc
dikshit673 <sachinnavodian@gmail.com>
2025-05-31 09:56:46 +0530

    {update}:added testimonial data

---
+++ client/src/data/testimonialArr.tsx
@@ -0,0 +1,195 @@
+// import testProfilePict from '@/assets/images/testimonials/user-1.jpg';
+
+export type TestimonialItemType = {
+  id: number;
+  name: string;
+  profession: string;
+  rating: number;
+  imgSrc: string;
+  description: string;
+};
+
+const testimonialArr: TestimonialItemType[] = [
+  {
+    id: 0,
+    name: 'john doe',
+    profession: 'student',
+    rating: 4.5,
+    imgSrc:
+      'https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 1,
+    name: 'cristina smith',
+    profession: 'actor',
+    rating: 3,
+    imgSrc:
+      'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 2,
+    name: 'da vinchi',
+    profession: 'scientist',
+    rating: 5,
+    imgSrc:
+      'https://images.unsplash.com/photo-1617945165020-140008b2f15c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzkwfHxjaGVmfGVufDB8fDB8fHwy',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 3,
+    name: 'pablo escobar',
+    profession: 'painter',
+    rating: 4,
+    imgSrc:
+      'https://images.unsplash.com/photo-1615567964485-0ee76b5b3410?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 4,
+    name: 'jane williams',
+    profession: 'footballer',
+    rating: 4.5,
+    imgSrc:
+      'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors3.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 5,
+    name: 'brad pitt',
+    profession: 'CEO',
+    rating: 4.5,
+    imgSrc:
+      'https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 6,
+    name: 'john doe',
+    profession: 'student',
+    rating: 2.5,
+    imgSrc:
+      'https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 7,
+    name: 'cristina smith',
+    profession: 'actor',
+    rating: 4.5,
+    imgSrc:
+      'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 8,
+    name: 'da vinchi',
+    profession: 'scientist',
+    rating: 5,
+    imgSrc:
+      'https://images.unsplash.com/photo-1617945165020-140008b2f15c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzkwfHxjaGVmfGVufDB8fDB8fHwy',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 9,
+    name: 'pablo escobar',
+    profession: 'painter',
+    rating: 3.5,
+    imgSrc:
+      'https://images.unsplash.com/photo-1615567964485-0ee76b5b3410?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 10,
+    name: 'jane williams',
+    profession: 'footballer',
+    rating: 5,
+    imgSrc:
+      'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors3.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 11,
+    name: 'brad pitt',
+    profession: 'CEO',
+    rating: 4.5,
+    imgSrc:
+      'https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 12,
+    name: 'john doe',
+    profession: 'student',
+    rating: 4,
+    imgSrc:
+      'https://images.unsplash.com/photo-1521119989659-a83eee488004?q=80&w=1923&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 13,
+    name: 'cristina smith',
+    profession: 'actor',
+    rating: 5,
+    imgSrc:
+      'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 14,
+    name: 'da vinchi',
+    profession: 'scientist',
+    rating: 3,
+    imgSrc:
+      'https://images.unsplash.com/photo-1617945165020-140008b2f15c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzkwfHxjaGVmfGVufDB8fDB8fHwy',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 15,
+    name: 'pablo escobar',
+    profession: 'painter',
+    rating: 3.5,
+    imgSrc:
+      'https://images.unsplash.com/photo-1615567964485-0ee76b5b3410?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 16,
+    name: 'jane williams',
+    profession: 'footballer',
+    rating: 4.5,
+    imgSrc:
+      'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/eourInstructors3.svg',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+  {
+    id: 17,
+    name: 'brad pitt',
+    profession: 'CEO',
+    rating: 4,
+    imgSrc:
+      'https://images.unsplash.com/photo-1610216705422-caa3fcb6d158?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque fugiat vel illum possimus aspernatur expedita adipisci placeat incidunt unde dignissimos.',
+  },
+];
+
+export { testimonialArr };
