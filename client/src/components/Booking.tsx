c0284cc815198ee360385f815fd7ad13865dde67
dikshit673 <sachinnavodian@gmail.com>
2025-05-31 09:53:51 +0530

    {update}:added booking for home route

---
+++ client/src/components/Booking.tsx
@@ -0,0 +1,71 @@
+import { memo, useCallback, useState } from 'react';
+import { Button, HType1, HType2 } from './ui';
+import { bookingInputsArr } from '@/data';
+
+import DinningTablePic from '@/assets/images/booking/dinning-1.jpeg';
+import { BookingFormType } from '@/types/Booking';
+import { FormPresenter } from './ui';
+
+const Booking = memo(() => {
+  const [bookForm, setBookForm] = useState<BookingFormType>({
+    name: '',
+    email: '',
+    phone: '',
+    datetime: '',
+    guests: '',
+    message: '',
+  });
+
+  const handleFormSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const { name, email, phone, datetime, guests } = bookForm;
+      if (!name || !email || !phone || !datetime || !guests) {
+        alert('Please fill all fields');
+        return;
+      }
+      alert('form submitted');
+      console.log('form submitted');
+    },
+    [bookForm]
+  );
+
+  return (
+    <section className='border-b border-b-gray-500 py-12'>
+      <div className='mx-auto w-9/10'>
+        <HType2 hypens='both'>Reservation</HType2>
+        <HType1 className='text-center'>book a table online</HType1>
+        <div className='grid grid-cols-1 gap-8 md:grid-cols-2'>
+          <div className='shadow-primary h-full rounded-lg bg-white'>
+            <img
+              src={DinningTablePic}
+              alt='reservation'
+              className='h-full w-full rounded-lg object-cover'
+            />
+          </div>
+          <div className='shadow-primary h-full rounded-lg bg-white'>
+            <div className='p-8'>
+              <HType1 tag='h4' className='text-prime-100 mb-2 text-center'>
+                booking
+              </HType1>
+              <form onSubmit={handleFormSubmit}>
+                <FormPresenter
+                  formName='booking'
+                  arr={bookingInputsArr}
+                  fields={bookForm}
+                  setFields={setBookForm}
+                />
+
+                <Button type='submit' className='my-5'>
+                  book now
+                </Button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+});
+
+export default Booking;
