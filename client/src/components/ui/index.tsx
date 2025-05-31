ffff2044d032bcb18995f2f3995f1c59f779d6c3
dikshit673 <sachinnavodian@gmail.com>
2025-05-31 09:46:17 +0530

    {update}:added barrel export for custum uis

---
+++ client/src/components/ui/index.tsx
@@ -0,0 +1,6 @@
+export { Button } from './Button';
+export { Counter } from './Counter';
+export { FormPresenter } from './FormPresenter';
+export { HType1, HType2 } from './Headings';
+export { Input, TextArea } from './Input';
+export { Swiper, SwiperItem } from './Swiper';
