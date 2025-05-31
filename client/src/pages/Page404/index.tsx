8e6c08aa5cc6ee019dd6a6f6ed6be4a8b88db069
dikshit673 <sachinnavodian@gmail.com>
2025-05-31 15:46:38 +0530

    [update]:added page 404

---
+++ client/src/pages/Page404/index.tsx
@@ -0,0 +1,12 @@
+import { memo } from 'react';
+
+const Page404 = memo(() => {
+  return (
+    <div className='text-center'>
+      <span>404</span>
+      <div>page not found</div>
+    </div>
+  );
+});
+
+export default Page404;
